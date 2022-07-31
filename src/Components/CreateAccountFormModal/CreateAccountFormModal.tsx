f2633fbc0c1fa147dff4eb36852e667ecae74e86
Youp <youpkuiper@gmail.com>
2022-07-31 22:25:46 +0200

    Added closebutton to modals

--- src/Components/CreateAccountFormModal/CreateAccountFormModal.tsx
+++ src/Components/CreateAccountFormModal/CreateAccountFormModal.tsx
@@ -22,7 +22,7 @@ const CreateAccountFormModal = ({ setShowCreateAccountModal, createUserAccount }
 
   const handleOnClose = (event: any) => {
     // Only close when background is clicked
-    if (event.target.id === 'container') {
+    if (event.target.id === 'container' || event.target.id === 'closebutton') {
       setShowCreateAccountModal(false);
     }
   }
@@ -39,7 +39,7 @@ const CreateAccountFormModal = ({ setShowCreateAccountModal, createUserAccount }
     console.log(`current errorMessages: ${JSON.stringify(errorMessages, null, 2)}`)
     console.log(`Closing: ${indexToRemove}`)
     setErrorMessages(errorMessages.filter((message) => message.index !== indexToRemove))
-  } 
+  }
 
   const handleCreateUserAccount = async (event: any) => {
     event.preventDefault();
@@ -76,7 +76,13 @@ const CreateAccountFormModal = ({ setShowCreateAccountModal, createUserAccount }
       flex justify-center items-center">
 
         <div className="bg-white max-w-md w-full space-y-8 p-10 rounded-md">
-          <div>
+          <div className="flex justify-between items-start rounded-t">
+            <button id='closebutton' type="button" onClick={handleOnClose} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
+              <svg aria-hidden="true" className="w-5 h-5 pointer-events-none" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+              <span className="sr-only pointer-events-none">Close modal</span>
+            </button>
+          </div>
+          <div style={{marginTop: "0"}}>
             <img
               className="mx-auto h-12 w-auto"
               src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
--- src/Components/LoginFormModal/LoginFormModal.tsx
+++ src/Components/LoginFormModal/LoginFormModal.tsx
@@ -15,7 +15,7 @@ const LoginFormModal = ({ setShowLoginModal, loginUser }: LoginFormModalProps) =
 
   const handleOnClose = (event: any) => {
     // Only close when background is clicked
-    if (event.target.id === 'container') {
+    if (event.target.id === 'container' || event.target.id === 'closebutton') {
       setShowLoginModal(false);
     }
   }
@@ -42,7 +42,13 @@ const LoginFormModal = ({ setShowLoginModal, loginUser }: LoginFormModalProps) =
       flex justify-center items-center">
 
         <div className="bg-white max-w-md w-full space-y-8 p-10 rounded-md">
-          <div>
+          <div className="flex justify-between items-start rounded-t">
+            <button id='closebutton' type="button" onClick={handleOnClose} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
+              <svg aria-hidden="true" className="w-5 h-5 pointer-events-none" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+              <span className="sr-only pointer-events-none">Close modal</span>
+            </button>
+          </div>
+          <div style={{marginTop: "0"}}>
             <img
               className="mx-auto h-12 w-auto"
               src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
@@ -50,76 +56,76 @@ const LoginFormModal = ({ setShowLoginModal, loginUser }: LoginFormModalProps) =
             />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
           </div>
-          { showSpinner ? <div className="mt-8 space-y-6"><LoadingSpinner/></div> :
-          <form className="mt-8 space-y-6" action="#" method="POST">
-            <input type="hidden" name="remember" defaultValue="true" />
-            <div className="rounded-md shadow-sm -space-y-px">
+          {showSpinner ? <div className="mt-8 space-y-6"><LoadingSpinner /></div> :
+            <form className="mt-8 space-y-6" action="#" method="POST">
+              <input type="hidden" name="remember" defaultValue="true" />
+              <div className="rounded-md shadow-sm -space-y-px">
+                <div>
+                  <label htmlFor="email-address" className="sr-only">
+                    Email address
+                  </label>
+                  <input
+                    id="email-address"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    value={emailAddress}
+                    onChange={e => setEmailAddress(e.target.value)}
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Email address"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password" className="sr-only">
+                    Password
+                  </label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Password"
+                  />
+                </div>
+              </div>
+
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <input
+                    id="remember-me"
+                    name="remember-me"
+                    type="checkbox"
+                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                  />
+                  <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                    Remember me
+                  </label>
+                </div>
+
+                <div className="text-sm">
+                  <a href="/#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                    Forgot your password?
+                  </a>
+                </div>
+              </div>
+
               <div>
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  value={emailAddress}
-                  onChange={e => setEmailAddress(e.target.value)}
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Email address"
-                />
+                <button
+                  onClick={handleLogin}
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                    <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                  </span>
+                  Sign in
+                </button>
               </div>
-              <div>
-                <label htmlFor="password" className="sr-only">
-                  Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  value={password}
-                  onChange={e => setPassword(e.target.value)}
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Password"
-                />
-              </div>
-            </div>
-
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <input
-                  id="remember-me"
-                  name="remember-me"
-                  type="checkbox"
-                  className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                />
-                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                  Remember me
-                </label>
-              </div>
-
-              <div className="text-sm">
-                <a href="/#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                  Forgot your password?
-                </a>
-              </div>
-            </div>
-
-            <div>
-              <button
-                onClick={handleLogin}
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                  <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
-                </span>
-                Sign in
-              </button>
-            </div>
-          </form>}
+            </form>}
         </div>
       </div>
     </>
