f58783c9491ab66e1bfe48a21100afbf981a27a5
Youp Kuiper <youpkuiper@gmail.com>
2022-10-27 18:36:16 +0700

    not needed to call window.localstorage

--- src/hooks.tsx
+++ src/hooks.tsx
@@ -2,13 +2,13 @@ import { useEffect, useState } from "react";
 
 export const useStickyState = (defaultValue: any, key: any) => {
     const [value, setValue] = useState(() => {
-        const stickyValue = window.localStorage.getItem(key);
+        const stickyValue = localStorage.getItem(key);
         return stickyValue !== null
             ? JSON.parse(stickyValue)
             : defaultValue;
     });
     useEffect(() => {
-        window.localStorage.setItem(key, JSON.stringify(value));
+        localStorage.setItem(key, JSON.stringify(value));
     }, [key, value]);
     return [value, setValue];
 }
